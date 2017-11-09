9b85f1073101cc9dae0c289a8d493a905f2f6f88
Eduard Urbach <e.urbach@gmail.com>
2017-11-09 10:27:45 +0100

    Added webp support

--- outputs/ImageFile.go
+++ outputs/ImageFile.go
@@ -3,10 +3,13 @@ package outputs
 import (
 	"errors"
 	"fmt"
+	"image"
 	"io/ioutil"
+	"os"
 	"path"
 
 	"github.com/aerogo/ipo/inputs"
+	"github.com/chai2010/webp"
 
 	"github.com/aerogo/ipo"
 )
@@ -20,6 +23,7 @@ type ImageFile struct {
 	BaseName  string
 	Format    string
 	Size      int
+	Quality   float32
 }
 
 // Write ...
@@ -33,13 +37,39 @@ func (file *ImageFile) Write(obj interface{}) error {
 	}
 
 	img := networkImage.Image()
-	fmt.Println(file.BaseName+extension, "|", img.Bounds().Dx(), "x", img.Bounds().Dy(), "|", len(networkImage.Data())/1024, "KB")
+	width := img.Bounds().Dx()
+	height := img.Bounds().Dy()
+	resizeRequired := file.Size != 0 && file.Size != width
+
+	fmt.Println(file.BaseName+extension, "|", width, "x", height, "|", len(networkImage.Data())/1024, "KB")
 
 	// Original file output
-	if file.Format == "" {
+	if file.Format == "" && !resizeRequired {
 		fullPath := path.Join(file.Directory, file.BaseName+extension)
 		return ioutil.WriteFile(fullPath, networkImage.Data(), 0644)
 	}
 
+	if file.Format == "webp" {
+		fullPath := path.Join(file.Directory, file.BaseName+".webp")
+		return SaveWebP(img, fullPath, file.Quality)
+	}
+
 	return nil
+}
+
+// SaveWebP saves an image as a file in WebP format.
+func SaveWebP(img image.Image, out string, quality float32) error {
+	file, writeErr := os.Create(out)
+
+	if writeErr != nil {
+		return writeErr
+	}
+
+	defer file.Close()
+
+	encodeErr := webp.Encode(file, img, &webp.Options{
+		Quality: quality,
+	})
+
+	return encodeErr
 }
