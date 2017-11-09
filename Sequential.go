956199e8b3bd7b0199f3ebcd3eefb42d3e44dabf
Eduard Urbach <e.urbach@gmail.com>
2017-11-09 13:30:27 +0100

    Minor change

--- Sequential.go
+++ Sequential.go
@@ -1,10 +1,5 @@
 package ipo
 
-import (
-	"fmt"
-	"sync"
-)
-
 // SequentialInputs tries each input sequentially and returns the first successful one.
 func SequentialInputs(inputs []Input) (obj interface{}, err error) {
 	for _, input := range inputs {
@@ -28,27 +23,5 @@ func SequentialOutputs(obj interface{}, outputs []Output) error {
 		}
 	}
 
-	return nil
-}
-
-// ParallelOutputs tries to write each output parallelly and returns when all outputs finished.
-func ParallelOutputs(obj interface{}, outputs []Output) error {
-	var wg sync.WaitGroup
-
-	for _, output := range outputs {
-		wg.Add(1)
-
-		go func(out Output) {
-			err := out.Write(obj)
-
-			if err != nil {
-				fmt.Println(err)
-			}
-
-			wg.Done()
-		}(output)
-	}
-
-	wg.Wait()
 	return nil
 }
